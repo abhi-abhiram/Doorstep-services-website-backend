816ecaa4b7fe64aea423650b4ffa1b062f4b4423
ABHIRAM REDDY <anukoolaabhiram@outlook.com>
2022-05-20 23:30:36 +0530

    initial setup

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import { config } from 'dotenv';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+  config({ path: '.env' });
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = (process.env.PORT as unknown as number) || 3000;
+const HOST = process.env.HOST as string;
+
+app.listen(PORT, HOST, () => {
+  console.log('server is listening on', `http://${HOST}:${PORT}`);
+});
